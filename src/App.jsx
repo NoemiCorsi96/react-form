cb0b0a845dd30ffc6d439ec6b027cee03588bcbd
NoemiCorsi96 <noemicorsi96@gmail.com>
2025-10-15 17:27:15 +0200

    do MilestoneI, create a list of titles, use map and bootstrap for style

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import titles from './data/titles'
+
 /*Milestone 1
 Creare una pagina che visualizzi una lista di articoli, mostrandone solo il titolo.
 Milestone 2
 Aggiungiamo in pagina un semplice form con un campo input in cui inserire il titolo di un 
 nuovo articolo del blog. Al submit del form, mostrare la lista degli articoli aggiornati.
-
+ 
 BONUS
-
+ 
 Aggiungere la possibilità di cancellare ciascun articolo utilizzando un'icona.
 Impostare il lavoro su più componenti. */
+
 function App() {
 
 
